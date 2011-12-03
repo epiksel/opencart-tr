1b8c2b2aaa639a4b613d6c0547dd769f2d3a6a04
eka77a@gmail.com <eka77a@gmail.com@326d1ac8-9b32-cb3d-eda7-2afc9a001a40>
2011-12-03 14:22:50 +0000

    ortaklık takip kodu oluşturma açıklama metnide yazım hatası ve açıklama düzeltmesi
    git-svn-id: http://opencart-tr.googlecode.com/svn/trunk@13 326d1ac8-9b32-cb3d-eda7-2afc9a001a40

--- ana_dizine_yukleyin/catalog/language/turkish/affiliate/tracking.php
+++ ana_dizine_yukleyin/catalog/language/turkish/affiliate/tracking.php
@@ -6,7 +6,7 @@ $_['heading_title']    = 'Takip Kodu Oluştur';
 
 // Text
 $_['text_account']     = 'Ortaklık Hesabım';
-$_['text_description'] = 'Takip kodu oluşturma araçı dışında oluşturduğunuz bağlantılar ile gelen müşterilerden gelir elde edemezsiniz. Bu yüzden %s web sitesine gönderdiğiniz müşterilerden gelir elde etmek için aşağıdaki takip kodu oluşturma araçları kullanabilirsiniz.';
+$_['text_description'] = 'Takip kodu oluşturma aracı dışında oluşturduğunuz bağlantılar ile gelen müşterilerden gelir elde edemezsiniz. Bu yüzden %s web sitesine gönderdiğiniz müşterilerden gelir elde etmek için aşağıdaki takip kodu oluşturma aracını kullanarak takip kodu oluşturunuz.';
 $_['text_code']        = '<b>Takip Kodunuz:</b>';
 $_['text_generator']   = '<b>Takip Bağlantısı Oluştur</b><br />Oluşturmak için ürün adını yazınız:';
 $_['text_link']        = '<b>Takip Bağlantısı:</b>';
