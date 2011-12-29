7bf5c32168b54009354156ab241c9b50a639fc60
eka77a@gmail.com <eka77a@gmail.com@326d1ac8-9b32-cb3d-eda7-2afc9a001a40>
2011-12-29 08:38:24 +0000

    Bir kaç düzeltme yapıldı.
    git-svn-id: http://opencart-tr.googlecode.com/svn/trunk@27 326d1ac8-9b32-cb3d-eda7-2afc9a001a40

--- ana_dizine_yukleyin/catalog/language/turkish/affiliate/logout.php
+++ ana_dizine_yukleyin/catalog/language/turkish/affiliate/logout.php
@@ -5,7 +5,7 @@
 $_['heading_title'] = 'Oturumu Kapat';
 
 // Text
-$_['text_message']  = '<p>Oturumunuz güvenli bir şekilde kapatıldı. Güvenli bir şekilde bilgisayarı terk edebilirsiniz.</p><p>Alışveriş sepetiniz kaydedildi, bir dahaki oturumunuz da kaldığınız yerden devam edebilirsiniz.</p>';
+$_['text_message']  = '<p>Ortaklık hesabınızdan çıkış yaptınız.</p>';
 $_['text_account']  = 'Ortaklık Hesabım';
 $_['text_logout']   = 'Oturumu Kapat';
 ?>
