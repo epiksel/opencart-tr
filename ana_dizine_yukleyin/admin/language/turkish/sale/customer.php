1a412ae9d5b92d84ed0ba1ad34b287c804d15536
eka77a@gmail.com <eka77a@gmail.com@326d1ac8-9b32-cb3d-eda7-2afc9a001a40>
2012-01-07 01:48:21 +0000

    Müşteri IP Karaliste özelliği için admin dil dosyası ve değişkenler eklendi.
    git-svn-id: http://opencart-tr.googlecode.com/svn/trunk@32 326d1ac8-9b32-cb3d-eda7-2afc9a001a40

--- ana_dizine_yukleyin/admin/language/turkish/sale/customer.php
+++ ana_dizine_yukleyin/admin/language/turkish/sale/customer.php
@@ -10,6 +10,8 @@ $_['text_default']          = 'Varsayılan';
 $_['text_approved']			= '%s hesabını onayladınız!';
 $_['text_wait']           	= 'Lütfen Bekleyiniz!';
 $_['text_balance']			= 'Bakiye:';
+$_['text_add_blacklist']    = 'Kara Liste\'ye Ekle';
+$_['text_remove_blacklist'] = 'Karaliste\'den Kaldır';
 
 // Column
 $_['column_name']			= 'Müşteri Adı';
---
+++ ana_dizine_yukleyin/admin/language/turkish/sale/customer_blacklist.php
@@ -0,0 +1,21 @@
+<?php
+/* Turkceye Ceviren eka7a - http://www.e-piksel.com */
+
+// Heading
+$_['heading_title']    = 'Müşteri IP Karaliste';
+
+// Text
+$_['text_success']     = 'Başarılı: Müşteri IP karaliste başarılı bir şekilde değiştirildi!';
+
+// Column
+$_['column_ip']        = 'IP';
+$_['column_customer']  = 'Müşteri';
+$_['column_action']    = 'Eylem';
+
+// Entry
+$_['entry_ip']         = 'IP:';
+
+// Error
+$_['error_permission'] = 'Uyarı: Müşteri IP karalistesini düzenleme iznine sahip değilsiniz!';
+$_['error_ip']         = 'IP adresi 1 ile 15 karakter arasında olmalı!';
+?>
