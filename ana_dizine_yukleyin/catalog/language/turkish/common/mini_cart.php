2dcb635f2e9eb43c758e90145b6e0a9f4c8688d7
eka77a@gmail.com <eka77a@gmail.com@326d1ac8-9b32-cb3d-eda7-2afc9a001a40>
2012-01-10 21:40:37 +0000

    git-svn-id: http://opencart-tr.googlecode.com/svn/trunk@33 326d1ac8-9b32-cb3d-eda7-2afc9a001a40

--- ana_dizine_yukleyin/catalog/language/turkish/common/mini_cart.php
+++ ana_dizine_yukleyin/catalog/language/turkish/common/mini_cart.php
@@ -2,8 +2,10 @@
 /* Turkceye Ceviren eka7a - http://www.e-piksel.com */
 
 // Text
+$_['text_cart']     = 'Sepetim';
+$_['text_items']    = '%s ürün - %s';
 $_['text_remove']   = 'Kaldır';
 $_['text_empty']    = 'Alışveriş sepetiniz boş!';
-$_['text_cart']     = 'Sepetim';
+//$_['text_cart']     = 'Sepetim';
 $_['text_checkout'] = 'Kasaya Git';
 ?>
