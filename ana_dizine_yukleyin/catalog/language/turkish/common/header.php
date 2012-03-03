b368fb47caa07991bd1b59282c41227079a5ed05
eka77a@gmail.com <eka77a@gmail.com@326d1ac8-9b32-cb3d-eda7-2afc9a001a40>
2012-03-03 12:42:01 +0000

    git-svn-id: http://opencart-tr.googlecode.com/svn/trunk@52 326d1ac8-9b32-cb3d-eda7-2afc9a001a40

--- ana_dizine_yukleyin/catalog/language/turkish/common/header.php
+++ ana_dizine_yukleyin/catalog/language/turkish/common/header.php
@@ -2,14 +2,12 @@
 /* Turkceye Ceviren eka7a - http://www.e-piksel.com */
 
 // Text
-$_['text_home']		= 'Anasayfa';
-$_['text_wishlist'] = 'Alışveriş Listem (%s)';
-$_['text_cart']		= 'Sepetim';
-$_['text_search']	= 'Ne Aramıştınız...';
-$_['text_welcome']	= 'Hoşgeldin ziyaretçi <a href="%s">giriş yapın</a> ya da <a href="%s">kayıt olun</a>.';
-$_['text_logged']	= '<a href="%s">%s</a> olarak oturum açtınız <b>(</b> <a href="%s">Çıkış yap</a> <b>)</b>';
-$_['text_account']	= 'Hesabım';
-$_['text_checkout'] = 'Kasaya Git';
-$_['text_language'] = 'Dil';
-$_['text_currency'] = 'Para Birimi';
+$_['text_home']				= 'Anasayfa';
+$_['text_wishlist']			= 'Alışveriş Listem (%s)';
+$_['text_shopping_cart']	= 'Sepetim';
+$_['text_search']			= 'Ne Aramıştınız...';
+$_['text_welcome']			= 'Hoşgeldin ziyaretçi <a href="%s">giriş yapın</a> ya da <a href="%s">kayıt olun</a>.';
+$_['text_logged']			= '<a href="%s">%s</a> olarak oturum açtınız <b>(</b> <a href="%s">Çıkış yap</a> <b>)</b>';
+$_['text_account']			= 'Hesabım';
+$_['text_checkout']			= 'Kasaya Git';
 ?>
