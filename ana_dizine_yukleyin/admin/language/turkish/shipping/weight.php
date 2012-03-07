ecf0324184444ce68977686d550d38fd0959f23f
eka77a@gmail.com <eka77a@gmail.com@326d1ac8-9b32-cb3d-eda7-2afc9a001a40>
2012-03-07 20:48:47 +0000

    yeni sipariş düzenleme sayfası için eksik çevirilerin çevirisi yapıldı.
    git-svn-id: http://opencart-tr.googlecode.com/svn/trunk@56 326d1ac8-9b32-cb3d-eda7-2afc9a001a40

--- ana_dizine_yukleyin/admin/language/turkish/shipping/weight.php
+++ ana_dizine_yukleyin/admin/language/turkish/shipping/weight.php
@@ -9,7 +9,7 @@ $_['text_shipping']    = 'Kargo Metodları';
 $_['text_success']     = 'Başarılı: ağırlık orantılı Kargo başarılı bir şekilde değiştirildi!';
 
 // Entry
-$_['entry_rate']       = 'Değer:<br /><span class="help">Örnek: 5:10.00,7:12.00 Ağırlık:Fiyat,Ağırlık:Fiyat, etc..</span>';
+$_['entry_rate']       = 'Değer:<br /><span class="help">Örnek: 5:10.00,7:12.00 Ağırlık:Fiyat,Ağırlık:Fiyat, gibi.. En yüksek ağırlığı ayarlamayı unutmayınız.</span>';
 $_['entry_tax_class']  = 'Vergi Oranı:';
 $_['entry_geo_zone']   = 'Genel Bölge:';
 $_['entry_status']     = 'Durumu:';
--- ana_dizine_yukleyin/catalog/language/turkish/affiliate/login.php
+++ ana_dizine_yukleyin/catalog/language/turkish/affiliate/login.php
@@ -6,11 +6,11 @@ $_['heading_title']                 = 'Ortaklık Programı';
 
 // Text
 $_['text_account']                  = 'Ortaklık Hesabım';
-$_['text_login']                    = 'Oturum Aç';
-$_['text_description']              = '<p>%s ortaklık programına kayıt olmak ücretsizdir. %s sitemizdeki ürünleri oluşturduğunuz takip kodunu kendi web sitenizde istediğiniz bir yere yerleştirerek gelir elde edebilirsiniz. Sizin bağlantılarınızdan gelen herhangi bir müşteri alışveriş yaptığında belirlediğimiz komisyon oranlarında sizde gelir elde edersiniz. Şuan ki geçerli komisyon oranımız %s\'dir.</p><p>Daha fazla bilgi için, SSS sayfasını ziyaret edebilir ya da Ortaklık Programı Kullanımı &amp; Koşullarına bakabilirsiniz.</p>';
+$_['text_login']                    = 'Oturum Açın';
+$_['text_description']              = '<p>%s ortaklık programına kayıt olmak ücretsizdir. %s sitemizdeki ürünleri, oluşturacağınız takip kodu ile kendi web sitenize yerleştirerek gelir elde edebilirsiniz. Sizin bağlantılarınızdan gelen herhangi bir müşteri alışveriş yaptığında belirlediğimiz komisyon oranlarında sizde gelir elde edersiniz. Şuan ki geçerli komisyon oranımız %s\'dir.</p><p>Daha fazla bilgi için, Ortaklık Programı Kullanımı &amp; Koşullarına bakabilirsiniz.</p>';
 $_['text_new_affiliate']            = 'Yeni Ortak';
-$_['text_register_account']         = '<p>Ben bir ortak değilim</p><p>Yeni bir ortaklık hesabı oluşturmak için lüften devam butonuna tıklayınız. Müşteri hesabınızın bağlı olmaması gerektiğini unutmayınız.</p>';
-$_['text_returning_affiliate']      = 'Ortaklık Oturumu';
+$_['text_register_account']         = '<p>Ben ortak değilim</p><p>Yeni ortaklık hesabı oluşturmak için lüften devam butonuna tıklayınız. Müşteri hesabınız ile bağlantılı olmadığını unutmayınız.</p>';
+$_['text_returning_affiliate']      = 'Oturum Aç';
 $_['text_i_am_returning_affiliate'] = 'Ortaklık için daha önce kayıt oldum.';
 $_['text_forgotten']                = 'Parolamı Unuttum';
 
@@ -19,5 +19,5 @@ $_['entry_email']                   = 'Ortaklık E-Posta:';
 $_['entry_password']                = 'Parola:';
 
 // Error
-$_['error_login']                  = 'Uyarı: E-Posta Adresi ya da parolanız yanlış';
+$_['error_login']                  = 'Uyarı: E-Posta adresi ya da parolanız yanlış';
 ?>
