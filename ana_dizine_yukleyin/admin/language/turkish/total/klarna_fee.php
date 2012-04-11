01ab2b256012681c90bab53aef3c52608296c2d9
eka77a <eka77a@326d1ac8-9b32-cb3d-eda7-2afc9a001a40>
2012-04-11 00:31:16 +0000

    total/klarna_fee.php içine dil değişkenleri eklendi.
    git-svn-id: http://opencart-tr.googlecode.com/svn/trunk@64 326d1ac8-9b32-cb3d-eda7-2afc9a001a40

---
+++ ana_dizine_yukleyin/admin/language/turkish/total/klarna_fee.php
@@ -0,0 +1,19 @@
+<?php
+/* Turkceye Ceviren eka7a - http://www.e-piksel.com */
+
+// Heading
+$_['heading_title']    = 'Klarna Ücreti';
+
+// Text
+$_['text_total']       = 'Sipariş Toplamları';
+$_['text_success']     = 'Başarılı: Klarna ücreti toplamı başarılı bir şekilde değiştirildi';
+
+// Entry
+$_['entry_total']      = 'Sipariş Toplamı:';
+$_['entry_fee']        = 'Ücreti:';
+$_['entry_tax_class']  = 'Vergi Oranı:';
+$_['entry_status']     = 'Durumu:';
+$_['entry_sort_order'] = 'Sıralama:';
+
+// Error
+$_['error_permission'] = 'Uyarı: Klarna ücreti toplamını düzenleme iznine sahip değilsiniz!';
