edc6d30bc3b907ae51e8d9cd244098b19c6380bb
eka77a <eka77a@326d1ac8-9b32-cb3d-eda7-2afc9a001a40>
2012-08-04 02:16:14 +0000

    git-svn-id: http://opencart-tr.googlecode.com/svn/trunk@85 326d1ac8-9b32-cb3d-eda7-2afc9a001a40

--- ana_dizine_yukleyin/catalog/language/turkish/checkout/success.php
+++ ana_dizine_yukleyin/catalog/language/turkish/checkout/success.php
@@ -2,12 +2,13 @@
 /* Turkceye Ceviren eka7a - http://www.e-piksel.com */
 
 // Heading
-$_['heading_title'] = 'Siparişiniz tamamlanıyor!';
+$_['heading_title']				= 'Siparişiniz başarıyla işleme alınmıştır!';
+$_['heading_title_customer']	= '#%s Nolu siparişiniz başarıyla işleme alınmıştır!';
 
 // Text
-$_['text_customer'] = '<p>Siparişiniz başarıyla tamamlandı!</p><p>Hesabınızı görüntülemek için <a href="%s">buraya</a> sipariş geçmişiniz için <a href="%s">buraya</a> tıklayınız.</p><p>Eğer indirilebilir ürün satın aldıysanız, <a href="%s">indirilebilir Ürünler</a> sayfasına bakınız.</p><p>Herhangi bir sorunuz varsa <a href="%s">buraya tıklayın</a>.</p><p>Bizi tercih ettiğiniz için teşekkür ederiz.</p>';
-$_['text_guest']    = '<p>Siparişiniz başarıyla tamamlandı!</p><p>Herhangi bir sorunuz varsa <a href="%s">buraya tıklayın</a>.</p><p>Bizi tercih ettiğiniz için teşekkür ederiz.</p>';
-$_['text_basket']   = 'Sepetim';
-$_['text_checkout'] = 'Kasaya Git';
-$_['text_success']  = 'Başarılı';
+$_['text_customer']				= '<p><a href="%s">#%s</a> Nolu siparişiniz işleme alınmıştır!</p><p>Hesabınızı görüntülemek için <a href="%s">buraya</a> sipariş geçmişiniz için <a href="%s">buraya</a> tıklayınız.</p><p>Eğer indirilebilir ürün satın aldıysanız, <a href="%s">indirilebilir Ürünler</a> sayfasına bakınız.</p><p>Herhangi bir sorunuz varsa <a href="%s">buraya tıklayın</a>.</p><p>Herhangi bir sorunuz varsa <a href="%s">buraya tıklayın</a>.</p><p>Bizi tercih ettiğiniz için teşekkür ederiz.</p>';
+$_['text_guest']				= '<p>#%s Nolu siparişiniz başarıyla işleme alınmıştır!</p><p>Herhangi bir sorunuz varsa <a href="%s">buraya tıklayın</a>.</p><p>Bizi tercih ettiğiniz için teşekkür ederiz.</p>';
+$_['text_basket']				= 'Sepetim';
+$_['text_checkout']				= 'Kasaya Git';
+$_['text_success']				= 'Başarılı';
 ?>
