1ce79c266acea243614e63ba5e871f7138451dec
Ekrem KAYA <webmaster@e-piksel.com>
2012-08-25 04:17:45 -0700

    Vqmod dil değişkenleri eklendi. sql dosyasına yeni ülkeler ve şehirler eklendi yazım düzeltmeleri yapıldı.

--- ana_dizine_yukleyin/admin/language/turkish/extension/manage.php
+++ ana_dizine_yukleyin/admin/language/turkish/extension/manage.php
@@ -4,6 +4,11 @@
 // Heading 
 $_['heading_title']    = 'Eklenti Yönetimi';
 
+// Text
+$_['text_success']     = 'Başarılı: Eklentiniz kuruldu!';
+
 // Error
 $_['error_permission'] = 'Uyarı: Eklenti yönetimini değiştirme iznine sahip değilsiniz!';
+$_['error_upload']     = 'Yükleme gerekli!';
+$_['error_filetype']   = 'Gerçersiz dosya türü!';
 ?>
