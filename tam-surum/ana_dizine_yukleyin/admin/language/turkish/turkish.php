c1b9d405608ef3da722d1898bdffd1daf98b62b8
Ekrem KAYA <webmaster@e-piksel.com>
2012-11-15 15:28:25 -0800

    Türkiye için para birimi nokta işaretleri düzenlendi.

--- tam-surum/ana_dizine_yukleyin/admin/language/turkish/turkish.php
+++ tam-surum/ana_dizine_yukleyin/admin/language/turkish/turkish.php
@@ -7,8 +7,8 @@ $_['direction']         	  = 'ltr';
 $_['date_format_short'] 	  = 'd/m/Y';
 $_['date_format_long']  	  = 'l dS F Y';
 $_['time_format']       	  = 'h:i:s A';
-$_['decimal_point']     	  = '.';
-$_['thousand_point']    	  = ',';
+$_['decimal_point']     	  = ',';
+$_['thousand_point']    	  = '.';
 
 // Text
 $_['text_yes']                = 'Evet';
