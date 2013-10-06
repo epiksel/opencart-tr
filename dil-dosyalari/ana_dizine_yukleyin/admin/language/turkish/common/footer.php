6694a949aeedc8705083d109256b6e738290220f
Ekrem KAYA <webmaster@e-piksel.com>
2013-10-06 16:45:04 +0300

    Yazım hatası

--- dil-dosyalari/ana_dizine_yukleyin/admin/language/turkish/common/footer.php
+++ dil-dosyalari/ana_dizine_yukleyin/admin/language/turkish/common/footer.php
@@ -2,5 +2,5 @@
 /* Turkceye Ceviren eka7a - http://www.e-piksel.com */
 
 // Text
-$_['text_footer'] = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved. | <a href="http://www.e-piksel.com" title="E-Piksel E-Ticaret ve Bilgi Teknolojiler">E-Piksel</a> &copy; 2009-' . date('Y') . ' Tüm Hakları Saklıdır.<br />Sürüm %s';
+$_['text_footer'] = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved. | <a href="http://www.e-piksel.com" title="E-Piksel E-Ticaret ve Bilgi Teknolojileri">E-Piksel</a> &copy; 2009-' . date('Y') . ' Tüm Hakları Saklıdır.<br />Sürüm %s';
 ?>
