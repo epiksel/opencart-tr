0d54eeeb804506377514d85ed555aa44a6b9b116
Ekrem KAYA <webmaster@e-piksel.com>
2014-01-28 16:00:40 +0200

    OpenCart TR logo güncellendi. Kurulum/Güncelleme yazılımı OpenCart v2.0 sürümüne güncellendi.

--- dil-dosyalari/ana_dizine_yukleyin/admin/language/turkish/mail/affiliate.php
+++ dil-dosyalari/ana_dizine_yukleyin/admin/language/turkish/mail/affiliate.php
@@ -5,9 +5,9 @@
 $_['text_approve_subject']		= '%s - Ortaklık Heabınız Etkinleştirildi!';
 $_['text_approve_welcome']		= '%s ortaklık programına kayıt olduğunuz için teşekkür ederiz!';
 $_['text_approve_login']		= 'Ortaklık hesabınız onaylanmıştır. E-Posta adresinizi ve şifrenizi kullanarak web sitemizi ziyaret ediniz veya aşağıdaki URL ile giriş yapabilirsiniz:';
-$_['text_approve_services']		= 'Oturum açtıktan sonra, takip kodu oluşturabilir, komisyon ödemelerini izleyebilir ve bilgilerinizi düzenleyebilirsiniz.';
+$_['text_approve_services']		= 'Oturum açtıktan sonra, takip kodu oluşturabilir, kazançlarınızı takip edebilir ve bilgilerinizi düzenleyebilirsiniz.';
 $_['text_approve_thanks']		= 'Teşekküler,';
-$_['text_transaction_subject']	= '%s - Ortaklık Komisyonunuz';
-$_['text_transaction_received']	= 'Komisyon oranınıza göre aldığınız bakiye %s dir.';
-$_['text_transaction_total']	= 'Aldığınız bakiye ile toplam bakiyeniz %s.';
+$_['text_transaction_subject']	= '%s - Ortaklık Komisyonu';
+$_['text_transaction_received']	= 'Komisyon oranınıza göre kazandığınız bakiye %s dir.';
+$_['text_transaction_total']	= 'Kazandığınız bakiye ile birlikte toplam bakiyeniz %s.';
 ?>
--- dil-dosyalari/ana_dizine_yukleyin/catalog/language/turkish/account/voucher.php
+++ dil-dosyalari/ana_dizine_yukleyin/catalog/language/turkish/account/voucher.php
@@ -9,7 +9,7 @@ $_['text_account']     = 'Hesabım';
 $_['text_voucher']     = 'Hediye Çeki';
 $_['text_description'] = 'Siparişinizin ödemesini yaptığınızda hediye çeki alıcısına gönderilecektir.';
 $_['text_agree']       = 'Hediye çekinin iade edilemeyeceğini biliyorum ve onaylıyorum';
-$_['text_message']     = '<p>Hediye çeki satın aldığınız için teşekkür ederiz! Siparişiniz tamamlandığında hediye çeki ile ilgili detaylar sahibine e-posta ile gönderilecektir.</p>';
+$_['text_message']     = '<p>Hediye çekini satın alabilmeniz için sepetinize eklendi! Siparişiniz tamamlandığında hediye çeki ile ilgili detaylar alıcı e-posta adresine gönderilecektir.</p>';
 $_['text_for']         = '%s tutarında %s için hediye çeki';
 
 // Entry
