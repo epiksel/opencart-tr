c1a23f4be28f720d7766ecb9b7ac8a1c86bce0c5
Ekrem KAYA <webmaster@e-piksel.com>
2019-01-05 21:28:35 +0300

    OpenCart v3.0.3.0 değişiklikleri

---
+++ dil-dosyalari/ana_dizine_yukleyin/admin/language/tr-tr/extension/extension/advertise.php
@@ -0,0 +1,18 @@
+<?php
+// Translator : Ekrem KAYA
+// Website    : https://e-piksel.com
+
+// Heading
+$_['heading_title']    = 'Reklamcılık Araçları';
+
+// Text
+$_['text_success']     = 'Başarılı: Reklamcılık Araçları güncellendi!';
+
+// Columns
+$_['column_name']      = 'Reklamcılık Adı';
+$_['column_status']    = 'Durumu';
+$_['column_action']    = 'Eylem';
+
+// Error
+$_['error_permission'] = 'Uyarı: Reklamcılık Araçlarını değiştirme iznine sahip değilsiniz!';
+$_['error_adblock']    = 'Reklam engelleyici kullanıyorsunuz gibi görünüyor. Reklamcılık bölümünü kullanmak için lütfen OpenCart yönetici panelinizde reklam engelleyicisini devre dışı bırakın.';
---
+++ dil-dosyalari/ana_dizine_yukleyin/catalog/language/tr-tr/extension/advertise/google.php
@@ -0,0 +1,7 @@
+<?php
+// Translator : Ekrem KAYA
+// Website    : https://e-piksel.com
+
+// Text
+$_['text_cron_email_message'] = '<p>Bu, Google Shopping uzantınız tarafından gerçekleştirilen en son CRON görevinin otomatik bir rapordur.</p><p>%s</p>';
+$_['text_cron_email_subject'] = 'CRONJOB Raporu - Google Shopping on OpenCart';
