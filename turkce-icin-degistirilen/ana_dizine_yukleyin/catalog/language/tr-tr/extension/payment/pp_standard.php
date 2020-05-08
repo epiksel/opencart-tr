b1fef6ca0cc16ae4853e8ddd1e8aa3caeda4a2df
Ekrem KAYA <webmaster@e-piksel.com>
2020-05-09 00:24:00 +0300

    OpenCart v3.3.3.3 değişiklikleri uygulandı. https://github.com/opencart/opencart/releases/tag/3.0.3.3

--- turkce-icin-degistirilen/ana_dizine_yukleyin/catalog/language/tr-tr/extension/payment/pp_standard.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Turkceye Ceviren eka7a - http://e-piksel.com */
-
-// Text
-$_['text_title']    = 'PayPal';
-$_['text_testmode'] = 'Uyarı!!! Ödeme metodu \'Test Modundadır\'. Hesabınıza tahsilat yapılmayacaktır.';
-$_['text_total']    = 'Kargolama, Ambalajlama, İndirimler & Vergiler';
