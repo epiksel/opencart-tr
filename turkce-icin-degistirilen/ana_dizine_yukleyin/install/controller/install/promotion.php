eaa334b42e36c499fff7b63d2bc97b302c6a962c
Ekrem KAYA <webmaster@e-piksel.com>
2020-05-09 01:05:53 +0300

    penCart v3.3.3.3 değişiklikleri uygulandı. https://github.com/opencart/opencart/releases/tag/3.0.3.3

---
+++ turkce-icin-degistirilen/ana_dizine_yukleyin/install/controller/install/promotion.php
@@ -0,0 +1,10 @@
+<?php
+class ControllerInstallPromotion extends Controller {
+	public function index() {
+		$html = '<script async src="https://adsenix.com/api/adsload.js"></script>';
+		$html .= '<div class="adsenix-preloader"></div>';
+		$html .= '<div class="adsenix-wrapper" data-adsenix-publisher="1" data-adsenix-group="3" data-adsenix-format="1"></div>';
+
+		return $html;
+	}
+}
--- turkce-icin-degistirilen/ana_dizine_yukleyin/install/language/tr-tr/install/step_4.php
+++ turkce-icin-degistirilen/ana_dizine_yukleyin/install/language/tr-tr/install/step_4.php
@@ -8,29 +8,29 @@ $_['heading_title']               = 'Kurulum Tamamlandı';
 $_['text_step_4']                 = 'Satışa başlamak için hazır!';
 $_['text_catalog']                = 'Mağazaya Git';
 $_['text_admin']                  = 'Yönetim Paneline Git';
-$_['text_loading']                = 'Loading modules...';
-$_['text_extension']              = 'visit the extensions store';
-$_['text_mail']                   = 'Join the mailing list';
-$_['text_mail_description']       = 'Stay informed of OpenCart updates and events.';
+$_['text_loading']                = 'Yükleniyor...';
+$_['text_extension']              = 'Eklenti mağazasını ziyaret edin';
+$_['text_mail']                   = 'Posta listesine katılın';
+$_['text_mail_description']       = 'Güncellemelerden ve kampanyalardan haberdar olun.';
 $_['text_openbay']                = 'OpenBay Pro gives merchants the ability to link their store with 3rd party markets like eBay and Amazon. Import orders, list items and handle shipping information direct from OpenCart...<a href="http://www.openbaypro.com/?utm_source=opencart_install&utm_medium=referral&utm_campaign=opencart_install" target="_blank">More information</a>';
 $_['text_amazon_pay']             = 'Amazon Pay is a trusted payment solution which allows customers to check in and out using information already stored in their Amazon account. It\'s fast, simple and secure....<a href="https://pay.amazon.com/uk/signup" target="_blank">More information</a>';
-$_['text_facebook']               = 'Like us on Facebook';
-$_['text_facebook_description']   = 'Tell us how much you like OpenCart!';
-$_['text_facebook_visit']         = 'Visit our Facebook page';
-$_['text_forum']                  = 'Community forums';
-$_['text_forum_description']      = 'Get help from other OpenCart users';
-$_['text_forum_visit']            = 'Visit our forums';
-$_['text_commercial']             = 'Commercial support';
-$_['text_commercial_description'] = 'Development services from OpenCart partners';
-$_['text_commercial_visit']       = 'Visit our partner page';
+$_['text_facebook']               = 'Bizi Facebook\'ta Beğenin';
+$_['text_facebook_description']   = 'Bize OpenCart\'ı ne kadar sevdiğini söyle!';
+$_['text_facebook_visit']         = 'Facebook Sayfamızı Ziyaret Edin';
+$_['text_forum']                  = 'Topluluk Forumları';
+$_['text_forum_description']      = 'Diğer OpenCart kullanıcılarından yardım alın';
+$_['text_forum_visit']            = 'Forumlarımızı ziyaret edin';
+$_['text_commercial']             = 'Ücretli Destek';
+$_['text_commercial_description'] = 'OpenCart servis hizmetleri';
+$_['text_commercial_visit']       = 'Destek sayfamızı ziyaret edin';
 $_['text_price']                  = 'Price';
-$_['text_view']                   = 'View details';
+$_['text_view']                   = 'Detayları göster';
 $_['text_download']               = 'Download';
 $_['text_downloads']              = 'Downloads';
 
 // Button
-$_['button_mail']                 = 'Join here';
-$_['button_setup']                = 'Set-up now';
+$_['button_mail']                 = 'Abone Ol';
+$_['button_setup']                = 'Şimdi kur';
 
 // Error
 $_['error_warning']               = 'install dizinini silmeyi unutmayınız!';
