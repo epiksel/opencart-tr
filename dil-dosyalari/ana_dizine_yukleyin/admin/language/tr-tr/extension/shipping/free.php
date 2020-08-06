fca488a524b826647ad2c9e7cacd81fc091f6a53
Ekrem KAYA <webmaster@e-piksel.com>
2020-08-06 17:29:42 +0300

    Auto invoice_prefix in sql

--- dil-dosyalari/ana_dizine_yukleyin/admin/language/tr-tr/extension/shipping/free.php
+++ dil-dosyalari/ana_dizine_yukleyin/admin/language/tr-tr/extension/shipping/free.php
@@ -17,7 +17,7 @@ $_['entry_status']     = 'Durumu';
 $_['entry_sort_order'] = 'Sıralama';
 
 // Help
-$_['help_total']       = 'Ücretsiz kargo modülünü kullanılabilir duruma getirmek için Alt-Toplam tutarını giriniz.';
+$_['help_total']       = 'Ücretsiz kargo seçeneğinin aktif olması için Alt-Toplam tutarını giriniz.';
 
 // Error
 $_['error_permission'] = 'Uyarı: Ücretsiz kargoyu düzenleme iznine sahip değilsiniz!';
