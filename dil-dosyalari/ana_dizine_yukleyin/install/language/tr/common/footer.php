c79cabaf11ce20d5ff5cbf8fee4a146086d596c4
Ekrem KAYA <webmaster@e-piksel.com>
2022-06-01 00:49:50 +0300

    Copyright tarih düzeltmesi

--- dil-dosyalari/ana_dizine_yukleyin/install/language/tr/common/footer.php
+++ dil-dosyalari/ana_dizine_yukleyin/install/language/tr/common/footer.php
@@ -6,4 +6,4 @@
 $_['text_project']       = 'Proje Anasayfa';
 $_['text_documentation'] = 'Dökümanlar';
 $_['text_support']       = 'Destek Forumları';
-$_['text_footer']        = '<a href="https://e-piksel.com">OpenCart Türkçe</a> &copy; 2009-' . date('Y') . ' Tüm hakları saklıdır.';
+$_['text_footer']        = '<a href="https://e-piksel.com">OpenCart Türkçe</a> &copy; 2011-' . date('Y') . ' Tüm hakları saklıdır.';
