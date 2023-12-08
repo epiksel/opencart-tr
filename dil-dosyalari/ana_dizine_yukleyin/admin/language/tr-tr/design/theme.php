5d735122cd44c5cd2adfbf4a05eec5bdfbf0cb15
kiziltasalperensalih <141634682+kiziltasalperensalih@users.noreply.github.com>
2023-12-09 00:07:51 +0300

    3.0.3.8 -> 3.0.3.9

--- dil-dosyalari/ana_dizine_yukleyin/admin/language/tr-tr/design/theme.php
+++ dil-dosyalari/ana_dizine_yukleyin/admin/language/tr-tr/design/theme.php
@@ -11,7 +11,7 @@ $_['text_store']        = 'Mağaza Seçin';
 $_['text_template']     = 'Şablon Seçin';
 $_['text_default']      = 'Varsayılan';
 $_['text_history']      = 'Tema Geçmişi';
-$_['text_twig']         = 'Tema editörü twig şablon dilini kullanır. You can read about <a href="http://twig.sensiolabs.org/documentation" target="_blank" class="alert-link">Buradan</a> Twig sözdizimini okuyabilirsiniz.';
+$_['text_twig']         = 'Tema editörü twig şablon dilini kullanır. You can read about <a href="http://twig.symfony.com/doc/" target="_blank" class="alert-link">Buradan</a> Twig sözdizimini okuyabilirsiniz.';
 
 // Column
 $_['column_store']      = 'Mağaza';
